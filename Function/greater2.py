1c388cb756990edacca34e8c8438d7d4236d649c
Aarati <aaratib4242gmail.com>
2026-03-13 22:33:18 +0545

    Function completed

--- Function/greater2.py
+++ Function/greater2.py
@@ -1,11 +1,11 @@
+print("Hello ")
+print("testing 123")
 def greater(a,b):
     if (a>b):
         print("a is greater")
     else:
         print("b is greater")
 
+
+greater(10,20)
     
-    greater(6,7)
-
-
-    greater(54,6)
