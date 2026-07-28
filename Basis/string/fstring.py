78f1b12a616b5d9ae6778c2854117d5ea3bba727
Aarati <aaratib4242gmail.com>
2026-07-28 11:06:32 +0545

    String Completed

---
+++ Basis/string/fstring.py
@@ -0,0 +1,6 @@
+first="Aarati"
+last="Bhatt"
+age=20
+print(f"Hello,my name is {first} and my last name is {last}. I am {age} years old. ")
+newone="Hello,my name is {first} and my last name is {last}. I am {age}years old.".format(first,last,age)
+print(newone)
---
+++ Basis/string/slicing.py
@@ -0,0 +1,18 @@
+string1="Farwestern University"
+sub1=string1[11:20]
+sub2=string1[:11]
+sub3=string1[11:]
+sub4=string1[:]
+sub5=string1[-10:-3]
+sub6=string1[2::2]
+sub7=string1[::-1]
+sub8=string1[5:18].upper()
+
+print("sub1 =", sub1,
+      "\nsub2 =", sub2,
+      "\nsub3 =", sub3,
+      "\nsub4 =", sub4,
+      "\nsub5 =", sub5,
+      "\nsub6 =", sub6,
+      "\nsub7 =", sub7,
+      "\nsub8 =", sub8)
